2ed00e19c22b173d2829678ee82ede0187563480
malic <m6024m@163.com>
2019-07-28 19:11:48 +0800

    add some regex functions

--- src/com/company/consoleMain.java
+++ src/com/company/consoleMain.java
@@ -1,11 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class consoleMain {
     public static void main(String[] argv)
     {
         String url ="http://openjudge.cn/user/785958/?page=2";
         String rawMsg=webManage.readWeb(url);
         String tableContent= webManage.matchRegex(rawMsg,"<div class=\"recently-submit\">(.*?)</div>");
-        System.out.println(tableContent.replace("<[^>]+>","\n"));
+
+        String rawTable=webManage.replaceRegex(tableContent,"\\s","");
+        System.out.println(rawTable);
+
+        List<String> arr=webManage.regexPattern2Lst(rawTable,"<tr>(.*?)</tr>");
+        for(String it :arr)
+        {
+            System.out.println(it);
+        }
     }
 }
--- src/com/company/webManage.java
+++ src/com/company/webManage.java
@@ -3,19 +3,39 @@ package com.company;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class webManage{
+    public static List<String> regexPattern2Lst(String rawMessage,String regex)
+    {
+        List<String>  arr=new ArrayList<>();
+        Pattern regexPatten = Pattern.compile(regex);
+        Matcher matchGroup = regexPatten.matcher(rawMessage);
+        System.out.println(matchGroup.groupCount());
+        if(matchGroup.find())
+        {
+            for(int i=1;i<matchGroup.groupCount();i++)
+                arr.add(matchGroup.group(i));
+        }
+        else
+        {
+            arr.add("");
+        }
+        return arr;
+    }
     public static String replaceRegex(String rawMessage,String regex,String target)
     {
         Pattern regexPatten = Pattern.compile(regex);
         Matcher matchGroup = regexPatten.matcher(rawMessage);
-        if(matchGroup.find())
-        {
-            //TODO: 1
+        StringBuffer res = new StringBuffer();
+        while(matchGroup.find()){
+            matchGroup.appendReplacement(res,target);
         }
-        return ;
+        matchGroup.appendTail(res);
+        return res.toString();
     }
 
     public static String matchRegex(String rawMessage,String regex)
