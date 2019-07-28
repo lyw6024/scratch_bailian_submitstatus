6557461de8413bbd7822f5aaa809251ed36daaed
malic <m6024m@163.com>
2019-07-28 17:54:31 +0800

    add regex parser

--- src/com/company/consoleMain.java
+++ src/com/company/consoleMain.java
@@ -3,6 +3,9 @@ package com.company;
 public class consoleMain {
     public static void main(String[] argv)
     {
-        System.out.println("Hello,world!");
+        String url ="http://openjudge.cn/user/785958/?page=2";
+        String rawMsg=webManage.readWeb(url);
+        String tableContent= webManage.matchRegex(rawMsg,"<div class=\"recently-submit\">(.*?)</div>");
+        System.out.println(tableContent.replace("<[^>]+>","\n"));
     }
 }
---
+++ src/com/company/webManage.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class webManage{
+    public static String replaceRegex(String rawMessage,String regex,String target)
+    {
+        Pattern regexPatten = Pattern.compile(regex);
+        Matcher matchGroup = regexPatten.matcher(rawMessage);
+        if(matchGroup.find())
+        {
+            //TODO: 1
+        }
+        return ;
+    }
+
+    public static String matchRegex(String rawMessage,String regex)
+    {
+        Pattern meanRegex = Pattern.compile(regex);
+        Matcher meanGp = meanRegex.matcher(rawMessage);
+        if (meanGp.find()) {
+            return meanGp.group(1);
+        }
+        else
+            {
+            return "";
+        }
+    }
+	public static String readWeb(String strUrl){
+        String webpageCtx;
+		try {
+            URL url = new URL(strUrl);
+            InputStream is = url.openStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader bf = new BufferedReader(isr);
+            String str;
+            webpageCtx = "";
+            while ((str = bf.readLine())!=null){
+                webpageCtx += str;
+            }
+            bf.close();
+            isr.close();
+            is.close();
+            return webpageCtx;
+        }
+        catch (MalformedURLException e){
+            System.out.println("Connect error");
+            return "";
+        }
+        catch (IOException e){
+            System.out.println("Input error");
+            return "";
+        }
+	}
+}
