0b53aebee0c93207743d8b4ca233e1bc716ad62a
malic <m6024m@163.com>
2019-07-31 21:14:35 +0800

    down data to file, offline analysis

--- src/com/company/consoleMain.java
+++ src/com/company/consoleMain.java
@@ -2,12 +2,13 @@ package com.company;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class consoleMain {
     public static void main(String[] argv)
     {
-
+        /*
         List <statusNode> statusList,currLst;
         statusList=new ArrayList<>();
 
@@ -17,7 +18,7 @@ public class consoleMain {
             statusList.add(it);
         }
 
-/*
+
         for(int i=2;i<=16;i++) {
             currLst = webManage.generator("http://openjudge.cn/user/785958/?page="+i);
             for (statusNode it : currLst)
@@ -26,8 +27,20 @@ public class consoleMain {
             }
         }
 
- */
 
         System.out.println(statusList.size());
+
+        for(statusNode it:statusList)
+        {
+            System.out.printf("%s|%s|%s|%d|%d|%d\n",it.title,it.language,it.status,it.runtime,it.memory,it.codeLength);
+        }
+
+
+         */
+        List<statusNode> sList= fileManager.getStatusNode();
+        System.out.println(sList.size());
+        
+
+
     }
 }
